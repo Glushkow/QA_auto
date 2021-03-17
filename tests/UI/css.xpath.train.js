3d3a2a8c6b797c25d0d99fc00939c9c72a0ad15f
Glushkow <73849882+Glushkow@users.noreply.github.com>
2021-03-17 17:19:40 +0300

    End chapter CSS and XPATH. css.xpath.train is complete

--- tests/UI/css.xpath.train.js
+++ tests/UI/css.xpath.train.js
@@ -6,10 +6,22 @@ describe("Locators search", () => {
         // failing the test
         return false
     })
-    it("Finding elements on page", () => {
+    let test = it("Finding elements on page", () => {
         cy.visit(`${Cypress.env('UIpracticeURl')}/training-ground`);
+        //train css
         cy.get('input[id="ipt2"]').should("exist");
-        cy.get('//button[id="@b4"]').should("exist")
-    });
+        //train xpath
+        cy.xpath('//button[@id="b4"]').should("exist")
+        cy.xpath('//div[@id="lower-logo"]//a[.="TechStep Academy"]').should("exist")
+        cy.xpath('//a[@href="#"]').should("exist")
+        //train css with 2 param
+        cy.get('nav[data-content-field="navigation"] [href="/workshops"]').should("exist")
+        //css empty field check
+        cy.get('input[id="ipt1"]').should("be.empty")
+        //css dropdown list has element
+        cy.get('select[id="sel1"]').should("have.value", "first")
+
+    })
+
 });
 
